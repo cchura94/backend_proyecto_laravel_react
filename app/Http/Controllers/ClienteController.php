cdb262b71146c7372d34eda01a677bdf04a2f691
cchura94 <cchura.cpc@gmail.com>
2023-05-30 21:13:13 -0400

    Pedidos api ultimos cambios

---
+++ app/Http/Controllers/ClienteController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cliente;
+use Illuminate\Http\Request;
+
+class ClienteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        // http://127.0.0.1:8000/api/cliente?page=2&limit=5&q=112233
+        $limit = isset($request->limit)? $request->limit : 10;
+        if(isset($request->q)){
+            // buscar al cliente
+            $clientes = Cliente::orWhere('nombre_completo', "like", "%".$request->q."%")
+                                    ->orWhere('ci_nit', "like", "%".$request->q."%")
+                                    ->orderBy('id', 'desc')
+                                    ->paginate($limit);
+        }else{
+             $clientes = Cliente::orderBy('id', 'desc')->paginate($limit);
+        }
+
+        return response()->json($clientes, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // validar
+        $request->validate([
+            "nombre_completo" => "required",
+            "ci_nit" => "required"
+        ]);
+        // guardar
+        $clie = new Cliente;
+        $clie->nombre_completo = $request->nombre_completo;
+        $clie->ci_nit = $request->ci_nit;
+        $clie->telefono = $request->telefono;
+        $clie->correo = $request->correo;
+        $clie->save();
+        // res
+        return response()->json(["mensaje" => "Cliente registrado"], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+         $cliente = Cliente::findOrFail($id);
+
+         return response()->json($cliente, 200);
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            "nombre_completo" => "required",
+            "ci_nit" => "required"
+        ]);
+
+        $clie = Cliente::findOrFail($id);
+        $clie->nombre_completo = $request->nombre_completo;
+        $clie->ci_nit = $request->ci_nit;
+        $clie->telefono = $request->telefono;
+        $clie->correo = $request->correo;
+        $clie->update();
+        
+        return response()->json(["mensaje" => "Cliente Modificado"], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $clie = Cliente::findOrFail($id);
+        $clie->delete();
+
+        return response()->json(["mensaje" => "Cliente Eliminado"], 200);
+    }
+}
--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -4,15 +4,32 @@ namespace App\Http\Controllers;
 
 use App\Models\Pedido;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class PedidoController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $pedidos = Pedido::orderBy('id', 'desc')->with(['cliente', 'user', 'productos'])->paginate(5);
+        $limit = isset($request->limit) ? $request->limit : 10;
+        if (isset($request->q)) {
+            $pedidos = Pedido::orWhere('fecha', 'like', "%" . $request->q . "%")
+                ->orWhereHas('cliente', function ($query) use ($request) {
+                    $query->where('nombre_completo', 'like', '%' . $request->q . '%');
+                })
+                ->orderBy('id', 'desc')
+                ->with(['cliente', 'user', 'productos'])
+                ->paginate($limit);
+        } else {
+
+            $pedidos = Pedido::orderBy('id', 'desc')
+                ->with(['cliente', 'user', 'productos'])
+                ->paginate($limit);
+        }
+
         return response()->json($pedidos, 200);
     }
 
@@ -21,7 +38,64 @@ class PedidoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // validacion
+        $request->validate([
+            "cliente_id" => "required",
+            "productos" => "required"
+        ]);
+        // transaccion
+        DB::beginTransaction();
+
+        try {
+                /*
+                    {
+                        cliente_id: 7,
+                        productos: [
+                            {producto_id: 5, cantidad: 2},
+                            {producto_id: 9, cantidad: 1},
+                            {producto_id: 2, cantidad: 3}
+                        ]
+                    }
+                    // $request->cliente_id
+                    // $request->productos[1]['producto_id']
+                    [
+                        cliente_id => 7,
+                        productos => [
+                            [producto_id => 5, cantidad => 2],
+                            [producto_id => 9, cantidad => 1],
+                            [producto_id => 2, cantidad => 3]
+                        ]
+                    ]
+                */
+            // registrar pedido
+            $pedido = new Pedido();
+            $pedido->fecha = date("Y-m-d H:i:s");
+            $pedido->estado = 1; // 1, 2, 3
+            $pedido->cliente_id = $request->cliente_id;
+            $pedido->user_id = Auth::user()->id;
+            $pedido->observaciones = "EN PROCESO";
+            $pedido->save();
+
+            // asignamos los productos al pedido
+            foreach ($request->productos as $prod) {
+                $producto_id = $prod["producto_id"];
+                $cantidad = $prod["cantidad"];
+                
+                $pedido->productos()->attach($producto_id, ["cantidad" => $cantidad]);
+            }
+
+            $pedido->estado = 2;
+            $pedido->observaciones = $request->observaciones;
+            $pedido->update();
+
+            DB::commit();
+
+            return response()->json(["mensaje" => "Pedido registrado"], 201);
+        } catch (\Exception $e) {
+            DB::rollback();
+            // something went wrong
+            return response()->json(["mensaje" => "Error al registrar el Pedido"], 500);
+        }
     }
 
     /**
